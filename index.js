f178ed3efa2b6e0d65e7ed0c70976acbd394cdcd
vedantp72 <vedantpatel72@gmail.com>
2024-03-07 18:03:05 +0530

    c

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
